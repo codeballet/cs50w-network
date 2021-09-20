1bb8776c7ad53dbaec5e80e42444cae3dbcb5519
johan <stjernholm@msn.com>
2021-09-20 16:26:26 +0800

    Refactor: change like button to form

--- network/static/network/likes.js
+++ network/static/network/likes.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // change eventlistener to listen for like-form instead of button
     document.querySelectorAll('.like-button').forEach(button => {
         button.onclick = function(e) {
             // get the post id from button id
@@ -11,13 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function likes(post_id) {
-    // TODO: add csrf token verification
+    // acquire csrf token
+    const csrftoken = getCookie('csrftoken');
 
+    // register the like
     fetch(`like/${post_id}`, {
-        method: 'POST'
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+            'X-CSRFToken': csrftoken
+        }
     })
     .then( response => response.json())
     .then(message => {
         console.log(message);
     })
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
